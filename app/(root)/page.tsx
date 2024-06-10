d8e37d4d6b7f6411116fd0b982538c287c2cb09f
Bo <ybly.1993@gmail.com>
2024-06-10 18:37:56 +0100

    Add right sidebar panel to main page layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,15 +2,16 @@ import React from 'react'
 
 import HeaderBox from '@/components/HeaderBox'
 import TotalBalanceBox from '@/components/TotalBalanceBox'
+import RightSidebar from '@/components/RightSidebar'
 
 const Home = () => {
     // for now...
-    const loggedIn = { firstName: 'John', lastName: 'Smith' }
+    const loggedIn = { firstName: 'John', lastName: 'Smith', email: 'john.smith@email.com' }
 
     return (
         <section className='home'>
             <div className='home-content'>
-                <div className='home-header'>
+                <header className='home-header'>
                     <HeaderBox
                         type='greeting'
                         title='Welcome'
@@ -23,8 +24,14 @@ const Home = () => {
                         totalBanks={1}
                         totalCurrentBalance={12500}
                     />
-                </div>
+                </header>
+
+                {/* RECENT TRANSACTIONS */}
+
             </div>
+
+            <RightSidebar user={loggedIn} transactions={[]} banks={[{ currentBalance: 12500 }, { currentBalance: 123 }]} />
+
         </section>
     )
 }
