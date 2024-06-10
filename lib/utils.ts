cd0e945bc7952910fd44e09beef909250fe18188
Bo <ybly.1993@gmail.com>
2024-06-10 14:35:43 +0100

    format amount as GBP

--- lib/utils.ts
+++ lib/utils.ts
@@ -66,9 +66,9 @@ export const formatDateTime = (dateString: Date) => {
 };
 
 export function formatAmount(amount: number): string {
-  const formatter = new Intl.NumberFormat("en-US", {
+  const formatter = new Intl.NumberFormat("en-GB", {
     style: "currency",
-    currency: "USD",
+    currency: "GBP",
     minimumFractionDigits: 2,
   });
 
