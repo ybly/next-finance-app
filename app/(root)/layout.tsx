879cc0bf658b2e9072288d063fda16c8d38e5374
Bo <ybly.1993@gmail.com>
2024-06-10 17:31:32 +0100

    Add mobileNav for mobile view

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,6 @@
+import Image from 'next/image';
 import Sidebar from '../../components/Sidebar'
+import MobileNav from '@/components/MobileNav';
 
 export default function RootLayout({
     children,
@@ -11,7 +13,20 @@ export default function RootLayout({
     return (
         <main className='flex h-screen w-full font-inter'>
             <Sidebar user={loggedIn} />
-            {children}
+
+            <div className='flex size-full flex-col'>
+                <div className='root-layout'>
+                    <Image src='/icons/logo.svg' width={30} height={30} alt='menu logo' />
+
+                    <div>
+                        <MobileNav user={loggedIn} />
+                    </div>
+                </div>
+
+                {children}
+
+            </div>
+
         </main>
     );
 }
