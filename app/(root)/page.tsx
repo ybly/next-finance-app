7f0185024fe824d81fb0b6ebec707e0498d4c935
Bo <ybly.1993@gmail.com>
2024-06-10 15:18:28 +0100

    Update page.tsx
    Update default layout to display header with total balances box

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,32 @@
 import React from 'react'
 
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
+
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+    // for now...
+    const loggedIn = { firstName: 'Bo' }
+
+    return (
+        <section className='home'>
+            <div className='home-content'>
+                <div className='home-header'>
+                    <HeaderBox
+                        type='greeting'
+                        title='Welcome'
+                        user={loggedIn?.firstName || 'Guest'}
+                        subtext='Access and manage your account and transactions.'
+                    />
+
+                    <TotalBalanceBox
+                        accounts={[]}
+                        totalBanks={1}
+                        totalCurrentBalance={12500}
+                    />
+                </div>
+            </div>
+        </section>
+    )
 }
 
 export default Home
