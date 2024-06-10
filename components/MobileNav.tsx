be4be22a8209d942505f147dc7bddb1af4b3c80c
Bo <ybly.1993@gmail.com>
2024-06-10 17:54:06 +0100

    Update MobileNav.tsx

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -1,28 +1,70 @@
+'use client'
+
 import React from 'react'
 
 import {
     Sheet,
     SheetContent,
-    SheetDescription,
-    SheetHeader,
-    SheetTitle,
+    SheetClose,
     SheetTrigger,
 } from "@/components/ui/sheet"
+import Image from 'next/image'
+import Link from 'next/link'
 
+import { usePathname } from 'next/navigation'
+
+import { sidebarLinks } from '@/constants'
+
+import { cn } from '@/lib/utils'
 
 const MobileNav = ({ user }: MobileNavProps) => {
+
+    const pathname = usePathname()
+
     return (
-        <section>
+        <section className='w-full max-w-[264px]'>
             <Sheet>
-                <SheetTrigger>Open</SheetTrigger>
-                <SheetContent>
-                    <SheetHeader>
-                        <SheetTitle>Are you absolutely sure?</SheetTitle>
-                        <SheetDescription>
-                            This action cannot be undone. This will permanently delete your account
-                            and remove your data from our servers.
-                        </SheetDescription>
-                    </SheetHeader>
+                <SheetTrigger>
+                    <Image src='/icons/hamburger.svg'
+                        width={30}
+                        height={30}
+                        alt='menu'
+                        className='cursor-pointer'
+                    />
+                </SheetTrigger>
+                <SheetContent side='left' className='border-none bg-white'>
+                    <Link href='/' className='cursor-pointer flex items-center gap-1 px-4'>
+                        <Image src='/icons/logo.svg' width={34} height={34} alt='logo' className='size-[24px] max-xl:size-14' />
+                        <h1 className='text-26 font-ibm-plex-serif font-bold text-black-1'>Next Bank</h1>
+                    </Link>
+
+                    <div className='mobilenav-sheet'>
+                        <SheetClose asChild>
+                            <nav className='flex h-full flex-col gap-6 pt-16 text-white'>
+                                {sidebarLinks.map(item => {
+                                    // Apply active class depending on the active route using shadcn 'cn' util
+                                    const isActive = pathname === item.route || pathname.startsWith(`${item.route}`)
+
+                                    return (
+                                        <SheetClose asChild key={item.label}>
+                                            <Link href={item.route} className={cn('mobilenav-sheet_close w-full', {
+                                                'bg-bank-gradient': isActive
+                                            })}>
+                                                <Image src={item.imgURL} width={20} height={20} alt={item.label} className={cn({ 'brightness-[3] invert-0': isActive })} />
+                                                <p className={cn('text-16 font-semibold text-black-2', { 'text-white': isActive })}>{item.label}</p>
+                                            </Link>
+
+                                        </SheetClose>
+
+                                    )
+                                })}
+
+                                {/* USER */}
+                            </nav>
+
+                            {/* FOOTER */}
+                        </SheetClose>
+                    </div>
                 </SheetContent>
             </Sheet>
 
