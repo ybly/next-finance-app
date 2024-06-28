d6053e75f5f25097982b99c50c0d23cb91929b24
Bo <ybly.1993@gmail.com>
2024-06-28 14:26:21 +0100

    Update appwrite.ts
    ignore .env warnings as we know we have parameters defined in the .env file

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -6,8 +6,8 @@ import { cookies } from 'next/headers';
 
 export async function createSessionClient() {
 	const client = new Client()
-		.setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT)
-		.setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT);
+		.setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
+		.setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!);
 
 	const session = cookies().get('appwrite-session');
 	if (!session || !session.value) {
@@ -25,9 +25,9 @@ export async function createSessionClient() {
 
 export async function createAdminClient() {
 	const client = new Client()
-		.setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT)
-		.setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT)
-		.setKey(process.env.NEXT_APPWRITE_KEY);
+		.setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
+		.setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!)
+		.setKey(process.env.NEXT_APPWRITE_KEY!);
 
 	return {
 		get account() {
