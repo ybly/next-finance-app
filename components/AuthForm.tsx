90b2561a57e4a4b15740d75fc215d7872e2bded7
Bo <ybly.1993@gmail.com>
2024-06-28 11:52:31 +0100

    Update AuthForm.tsx
    Add sign-up and login navigation in footer

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -17,10 +17,12 @@ import {
 
 import CustomFormInput from './CustomFormInput'
 import { authFormSchema } from '@/lib/utils'
+import { Loader2 } from 'lucide-react'
 
 
 const AuthForm = ({ type }: { type: string }) => {
     const [user, setUser] = useState(null)
+    const [isLoading, setIsLoading] = useState(false)
 
     // 1. Define your form.
     const form = useForm<z.infer<typeof authFormSchema>>({
@@ -35,7 +37,9 @@ const AuthForm = ({ type }: { type: string }) => {
     function onSubmit(values: z.infer<typeof authFormSchema>) {
         // Do something with the form values.
         // ✅ This will be type-safe and validated.
+        setIsLoading(true)
         console.log(values)
+        setIsLoading(false)
     }
 
     return (
@@ -88,9 +92,23 @@ const AuthForm = ({ type }: { type: string }) => {
                                 placeholder='Enter your password'
                             />
 
-                            <Button type="submit">Submit</Button>
+                            <div className='flex flex-col gap-4'>
+                                <Button type="submit" className='form-btn' disabled={isLoading}>
+                                    {
+                                        isLoading ?
+                                            <>
+                                                <Loader2 size={20} className='animate-spin' /> &nbsp; Loading...
+                                            </> : type === 'login' ? 'Login' : 'Sign-Up'
+                                    }
+                                </Button>
+                            </div>
                         </form>
                     </Form>
+
+                    <footer className='flex justify-center gap-1'>
+                        <p className='text-14 font-normal text-gray-600'>{type === 'login' ? "Don't have an account?" : 'Already have an account?'}</p>
+                        <Link className='form-link' href={type === 'login' ? '/sign-up' : '/login'} >{type === 'login' ? 'Sign Up' : 'Login'}</Link>
+                    </footer>
                 </>
             )}
 
