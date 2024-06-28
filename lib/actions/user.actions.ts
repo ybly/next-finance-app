09f80a240d585a0f0b711b4f7e6d3d359dd169a5
Bo <ybly.1993@gmail.com>
2024-06-28 14:37:17 +0100

    Create signup action with appwrite
    The signUpWithEmail function is an async function that takes the form data as an argument. It uses the createAdminClient function to create an admin Appwrite client and then calls the createEmailPasswordSession method on the account object. This method takes the email and password as arguments and returns a session object. We then set the session secret in a cookie and redirect the user to the account page.
    
    https://appwrite.io/docs/tutorials/nextjs-ssr-auth/step-5

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,6 +1,9 @@
 'use server';
 
-import { createSessionClient } from '../appwrite';
+import { ID } from 'node-appwrite';
+import { createAdminClient, createSessionClient } from '../appwrite';
+import { cookies } from 'next/headers';
+import { parseStringify } from '../utils';
 
 export const login = async () => {
 	try {
@@ -11,8 +14,28 @@ export const login = async () => {
 };
 
 export const signUp = async (userData: SignUpParams) => {
+	const { email, password, firstName, lastName } = userData;
+	// create user account
 	try {
-		// create user account
+		const { account } = await createAdminClient();
+
+		const newUserAccount = await account.create(
+			ID.unique(),
+			email,
+			password,
+			`${firstName} ${lastName}`
+		);
+
+		const session = await account.createEmailPasswordSession(email, password);
+
+		cookies().set('appwrite-session', session.secret, {
+			path: '/',
+			httpOnly: true,
+			sameSite: 'strict',
+			secure: true,
+		});
+
+		return parseStringify(newUserAccount);
 	} catch (error) {
 		console.error('Error', error);
 	}
