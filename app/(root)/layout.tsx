5cf68ee810555fb899c7b7d7052cb644cc752ded
Bo <ybly.1993@gmail.com>
2024-07-07 01:23:34 +0100

    redirect to login page automatically if no active session is logged in

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,14 +2,16 @@ import Image from 'next/image';
 import Sidebar from '../../components/Sidebar'
 import MobileNav from '@/components/MobileNav';
 import { getLoggedInUser } from '@/lib/actions/user.actions';
+import { redirect } from 'next/navigation';
 
 export default async function RootLayout({
     children,
 }: Readonly<{
     children: React.ReactNode;
 }>) {
-
     const loggedIn = await getLoggedInUser()
+
+    if (!loggedIn) redirect('/login')
 
     return (
         <main className='flex h-screen w-full font-inter'>
