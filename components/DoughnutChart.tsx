0493e5a53caac29a7528b18d06566dffc156d024
Bo <ybly.1993@gmail.com>
2024-07-26 16:44:43 +0100

    Update DoughnutChart.tsx
    read chart data from bank accounts db

--- components/DoughnutChart.tsx
+++ components/DoughnutChart.tsx
@@ -6,15 +6,18 @@ import { Doughnut } from "react-chartjs-2";
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 const DoughnutChart = ({ accounts }: DoughnutChartProps) => {
+    const accountNames = accounts.map((a) => a.name)
+    const balances = accounts.map((a) => a.currentBalance)
+
     const data = {
         datasets: [
             {
                 label: "Banks",
-                data: [1250, 2500, 3750],
+                data: balances,
                 backgroundColor: ['#0747B6', '#2265D8', '#2F91FA']
             }
         ],
-        labels: ['Bank 1', 'Bank 2', 'Bank3']
+        labels: accountNames
     }
 
     return (
