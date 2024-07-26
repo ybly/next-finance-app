a5b4a110d36339178f09b535183f1eb33e09aa6b
Bo <ybly.1993@gmail.com>
2024-07-26 11:17:23 +0100

    Add bank actions
    Add user banks queries actions to retrieve details from Appwrite DB by userId or documentId

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,6 +1,6 @@
 'use server';
 
-import { ID } from 'node-appwrite';
+import { ID, Query } from 'node-appwrite';
 import { createAdminClient, createSessionClient } from '../appwrite';
 import { cookies } from 'next/headers';
 import { encryptId, extractCustomerIdFromUrl, parseStringify } from '../utils';
@@ -236,5 +236,37 @@ export const exchangePublicToken = async ({
 		});
 	} catch (error) {
 		console.error('An error occurred while creating exchanging token: ', error);
+	}
+};
+
+export const getBanks = async ({ userId }: getBanksProps) => {
+	try {
+		const { database } = await createAdminClient();
+
+		const banks = await database.listDocuments(
+			DATABASE_ID!,
+			BANK_COLLECTION_ID!,
+			[Query.equal('userId', [userId])]
+		);
+
+		return parseStringify(banks.documents);
+	} catch (error) {
+		console.error('An error occurred while retrieving user banks: ', error);
+	}
+};
+
+export const getBank = async ({ documentId }: getBankProps) => {
+	try {
+		const { database } = await createAdminClient();
+
+		const banks = await database.listDocuments(
+			DATABASE_ID!,
+			BANK_COLLECTION_ID!,
+			[Query.equal('$id', [documentId])]
+		);
+
+		return parseStringify(banks.documents[0]);
+	} catch (error) {
+		console.error('An error occurred while retrieving user bank: ', error);
 	}
 };
