18218ea6423099ba5bc8e4e1ed718b6d9af8f7a7
Bo <ybly.1993@gmail.com>
2024-07-30 14:04:06 +0100

    fix typo

--- app/(root)/payment-transfer/page.tsx
+++ app/(root)/payment-transfer/page.tsx
@@ -1,7 +1,20 @@
 import HeaderBox from '@/components/HeaderBox'
+import PaymentTransferForm from '@/components/PaymentTransferForm'
+import { getAccounts } from '@/lib/actions/bank.actions'
+import { getLoggedInUser } from '@/lib/actions/user.actions'
 import React from 'react'
 
-const Transfer = () => {
+const Transfer = async () => {
+    const loggedInUser = await getLoggedInUser()
+    const accounts = await getAccounts({ userId: loggedInUser.$id })
+
+    if (!accounts) {
+        console.error('Accounts not found!', loggedInUser)
+        return
+    }
+
+    const accountsData = accounts?.data
+
     return (
         <section className='payment-transfer'>
             <HeaderBox
@@ -11,7 +24,7 @@ const Transfer = () => {
 
             <section className='size-full pt-5'></section>
 
-
+            <PaymentTransferForm accounts={accountsData} />
 
         </section>
     )
