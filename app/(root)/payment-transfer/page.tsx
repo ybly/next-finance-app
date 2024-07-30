0f3a1e0be3b10ac966af0f1f3217399785acf8d9
Bo <ybly.1993@gmail.com>
2024-07-30 13:54:41 +0100

    Payment Transfer page (WIP)

--- app/(root)/payment-transfer/page.tsx
+++ app/(root)/payment-transfer/page.tsx
@@ -1,8 +1,19 @@
+import HeaderBox from '@/components/HeaderBox'
 import React from 'react'
 
 const Transfer = () => {
     return (
-        <div>Transfer</div>
+        <section className='payment-transfer'>
+            <HeaderBox
+                title='Payment Transfer'
+                subtext='Please Provide any specific details or notes related to the payment transfer.'
+            />
+
+            <section className='size-full pt-5'></section>
+
+
+
+        </section>
     )
 }
 
