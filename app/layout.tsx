9900f5ed0095b6257c53c3886bf72b9d0b6c09e4
Bo <ybly.1993@gmail.com>
2024-07-30 15:22:08 +0100

    Add force-dynamic
    force dynamic link so pages are not rendered statically

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,15 @@
+export const dynamic = 'force-dynamic'
+
 import type { Metadata } from "next";
 import { Inter, IBM_Plex_Serif } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"], variable: '--font-inter' });
-const ibmPlexSerif = IBM_Plex_Serif({ 
-  subsets: ["latin"], 
+const ibmPlexSerif = IBM_Plex_Serif({
+  subsets: ["latin"],
   weight: ['400', '700'],
-  variable: '--font-ibm-plex-serif' });
+  variable: '--font-ibm-plex-serif'
+});
 
 export const metadata: Metadata = {
   title: "NextJS Finance Management App",
