96e81c1a3f26a3eff565caae4ac0a24517b90e28
Bo <ybly.1993@gmail.com>
2024-07-30 12:27:11 +0100

    Add styling to plaid link option button

--- components/PlaidLink.tsx
+++ components/PlaidLink.tsx
@@ -4,6 +4,7 @@ import { useRouter } from 'next/navigation';
 
 import { PlaidLinkOnSuccess, PlaidLinkOptions, usePlaidLink } from 'react-plaid-link'
 import { createLinkToken, exchangePublicToken } from '@/lib/actions/user.actions';
+import Image from 'next/image';
 
 const PlaidLink = ({ user, variant }: PlaidLinkProps) => {
     const router = useRouter();
@@ -45,12 +46,14 @@ const PlaidLink = ({ user, variant }: PlaidLinkProps) => {
                     Connect Bank
                 </Button>
             ) : variant === 'ghost' ? (
-                <Button>
-                    Connect Bank
+                <Button variant='ghost' onClick={() => open()} className='plaidlink-ghost'>
+                    <Image src='/icons/connect-bank.svg' alt='connect bank' height={24} width={24} />
+                    <p className='hidden text-[16px] font-semibold text-black-2 xl:block'>Connect Bank</p>
                 </Button>
             ) : (
-                <Button>
-                    Connect Bank
+                <Button onClick={() => open()} className='plaidlink-default'>
+                    <Image src='/icons/connect-bank.svg' alt='connect bank' height={24} width={24} />
+                    <p className='hidden text-[16px] font-semibold text-black-2 xl:block'>Connect Bank</p>
                 </Button>
             )}
         </>
